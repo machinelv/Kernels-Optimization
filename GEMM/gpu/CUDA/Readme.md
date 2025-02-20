7f721f8274e377d1feee515797a6f602f05986f4
machinelv <machinelvqz@outlook.com>
2025-02-20 10:59:53 +0800

    Finish v02.1.

--- GEMM/gpu/CUDA/Readme.md
+++ GEMM/gpu/CUDA/Readme.md
@@ -13,6 +13,8 @@ The project uses the execution and timing structure of Leimao's realization. And
 
 # Log
 
+- [ ] Use nsight to analyse the memory coalescing.
+
 ## Phase I
 
 ### V00
@@ -60,3 +62,43 @@ Custom GEMM VS cuBLAS GEMM Performance: 15.7657%
 
 ### v02
 
+#### v02.1
+```bash
+Device Name: Tesla V100-SXM3-32GB
+Memory Size: 31.7394 GB
+Peak Bandwitdh: 980.992 GB/s
+
+Matrix Size: M = 4096 N = 4096 K = 4096
+Matrix A: 4096 x 4096 Leading Dimension Size = 4096
+Matrix B: 4096 x 4096 Leading Dimension Size = 4096
+Matrix C: 4096 x 4096 Leading Dimension Size = 4096
+
+Custom GEMM Kernel V02
+cuBLAS GEMM Kernel Performance
+Latency: 12.1795 ms
+Effective Bandwidth: 16.53 GB/s
+Effective TFLOPS: 11.2845 TFLOPS
+Custom GEMM Kernel Performance
+Latency: 42.7428 ms
+Effective Bandwidth: 4.71019 GB/s
+Effective TFLOPS: 3.21549 TFLOPS
+Custom GEMM VS cuBLAS GEMM Performance: 28.4948%
+
+Matrix Size: M = 8192 N = 4096 K = 4096
+Matrix A: 8192 x 4096 Leading Dimension Size = 4096
+Matrix B: 4096 x 4096 Leading Dimension Size = 4096
+Matrix C: 8192 x 4096 Leading Dimension Size = 4096
+
+Custom GEMM Kernel V02
+cuBLAS GEMM Kernel Performance
+Latency: 23.7763 ms
+Effective Bandwidth: 14.1126 GB/s
+Effective TFLOPS: 11.561 TFLOPS
+Custom GEMM Kernel Performance
+Latency: 85.3269 ms
+Effective Bandwidth: 3.93246 GB/s
+Effective TFLOPS: 3.22147 TFLOPS
+Custom GEMM VS cuBLAS GEMM Performance: 27.8649%
+```
+
+Partition the matrix A and matrix B into tiles. While in version 02.1, we only partition the matrices once. And there is a bug in the program: the size of M, N, K tiles can't be changed. This bug will be fixed in version 02.2
--- GEMM/gpu/CUDA/src/02_2d_block_tiling.cu
+++ GEMM/gpu/CUDA/src/02_2d_block_tiling.cu
@@ -11,22 +11,43 @@
 
 
 // Coalesced read and write from global memory.
-template <typename T, size_t BLOCK_TILE_SIZE_X, size_t BLOCK_TILE_SIZE_Y,
+template <typename T, size_t BLOCK_TILE_SIZE_M, size_t BLOCK_TILE_SIZE_N,
           size_t BLOCK_TILE_SIZE_K>
 __global__ void gemm_v02(size_t m, size_t n, size_t k, T alpha, T const* A,
-                         size_t lda, T const* B, size_t ldb, T beta, T* C,
-                         size_t ldc)
-{
-    __shared__ T A_block_tile[BLOCK_TILE_SIZE_Y][BLOCK_TILE_SIZE_K];
-    __shared__ T B_block_tile[BLOCK_TILE_SIZE_K][BLOCK_TILE_SIZE_X];
-    // load data into shared memory
+                         size_t lda, T const* B, size_t ldb, T beta, T* C, size_t ldc) {
+    __shared__ T A_block_tile[BLOCK_TILE_SIZE_M][BLOCK_TILE_SIZE_K];
+    __shared__ T B_block_tile[BLOCK_TILE_SIZE_K][BLOCK_TILE_SIZE_N];
     
+    // Compute the A's and B's block tile index, which is same among all threads within a block
+    unsigned int const A_block_tile_id{blockIdx.y};
+    unsigned int const B_block_tile_id{blockIdx.x};
+    unsigned int const K_block_tile_num{(k + BLOCK_TILE_SIZE_K - 1) / BLOCK_TILE_SIZE_K};
+
+    unsigned int const c_row_idx{A_block_tile_id * BLOCK_TILE_SIZE_M + threadIdx.y};
+    unsigned int const c_col_idx{B_block_tile_id * BLOCK_TILE_SIZE_N + threadIdx.x};
     
-    
+    T sum_thread{0};
 
-    // compute the matrix multiplication
+    //Move K_block tile in the matrix A and matrix B
+    unsigned int K_block_tile_id{0};
+    while (K_block_tile_id < K_block_tile_num) {
+        // Load A and B into block_tile
+        unsigned int K_start{K_block_tile_id * BLOCK_TILE_SIZE_K};
+        A_block_tile[threadIdx.y][threadIdx.x] = A[c_row_idx * lda + K_start + threadIdx.x];
+        B_block_tile[threadIdx.y][threadIdx.x] = B[(K_start + threadIdx.y) * ldb  + c_col_idx];
+        K_block_tile_id++;
+        __syncthreads();
+        // Compute the sum
+        for (size_t k_thread{0}; k_thread < BLOCK_TILE_SIZE_K; ++k_thread) {
+            sum_thread += A_block_tile[threadIdx.y][k_thread] * B_block_tile[k_thread][threadIdx.x];
+        }
+        __syncthreads();
+    }
 
-
+    // Store the result
+    if (c_row_idx < m && c_col_idx < n) {
+        C[c_row_idx * ldc + c_col_idx] = alpha * sum_thread + beta * C[c_row_idx * ldc + c_col_idx];
+    }
 
 } 
 
@@ -38,17 +59,18 @@ void launch_gemm_kernel_v02(size_t m, size_t n, size_t k, T const* alpha,
 {
     // Feel free to play with the block tile sizes.
     // The algorithm correctness should always be guaranteed.
-    constexpr unsigned int BLOCK_TILE_SIZE_X{32U};
-    constexpr unsigned int BLOCK_TILE_SIZE_Y{32U};
+    constexpr unsigned int BLOCK_TILE_SIZE_M{32U};
+    constexpr unsigned int BLOCK_TILE_SIZE_N{32U};
     constexpr unsigned int BLOCK_TILE_SIZE_K{32U};
-    constexpr unsigned int NUM_THREADS{BLOCK_TILE_SIZE_X * BLOCK_TILE_SIZE_Y};
-    static_assert(BLOCK_TILE_SIZE_K * BLOCK_TILE_SIZE_Y % NUM_THREADS == 0U);
-    static_assert(BLOCK_TILE_SIZE_X * BLOCK_TILE_SIZE_K % NUM_THREADS == 0U);
-    dim3 const block_dim{BLOCK_TILE_SIZE_X, BLOCK_TILE_SIZE_Y, 1U};
+    constexpr unsigned int NUM_THREADS{BLOCK_TILE_SIZE_M * BLOCK_TILE_SIZE_N};
+    static_assert(NUM_THREADS <= 1024U);
+    static_assert(BLOCK_TILE_SIZE_K * BLOCK_TILE_SIZE_N % NUM_THREADS == 0U);
+    static_assert(BLOCK_TILE_SIZE_M * BLOCK_TILE_SIZE_K % NUM_THREADS == 0U);
+    dim3 const block_dim{BLOCK_TILE_SIZE_N, BLOCK_TILE_SIZE_M, 1U};
     dim3 const grid_dim{
         (static_cast<unsigned int>(n) + block_dim.x - 1U) / block_dim.x,
         (static_cast<unsigned int>(m) + block_dim.y - 1U) / block_dim.y, 1U};
-    gemm_v02<T, BLOCK_TILE_SIZE_X, BLOCK_TILE_SIZE_Y, BLOCK_TILE_SIZE_K>
+    gemm_v02<T, BLOCK_TILE_SIZE_M, BLOCK_TILE_SIZE_N, BLOCK_TILE_SIZE_K>
         <<<grid_dim, block_dim, 0U, stream>>>(m, n, k, *alpha, A, lda, B, ldb,
                                               *beta, C, ldc);
     CHECK_LAST_CUDA_ERROR();
